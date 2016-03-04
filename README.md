dbe3d3f119a08d5f5d8021c00d9bf9621fe1dc4b
futuresun912 <futuresun912@gmail.com>
2016-03-04 13:12:53 +0900

    v0.3

---
+++ README.md
@@ -0,0 +1,17 @@
+# MLSF
+Multi-Label Classification by Mining Meta-Label Specific Features.
+
+This is a example program for conduct experiments in [1].
+
+The program includes five multi-label classification methods:
+
+[1] MLSF: Lu Sun et al. Multi-Label Classification by Mining Meta-Label Specific Features. Submission to ICPR-16.
+[2] BR: M.R. Boutel et al. Learning multi-label scene classification. Pattern Recognition, 2004.
+[3] MDDM: Y. Zhang et al. Multilabel dimensionality reduction via dependence maximization. ACM Trans. Knowl. Discov. Data, 2010.
+[4] LIFT: M. Zhang. LIFT: Multi-label learning with label-specific features. PAMI, 2015.
+[5] LLSF: J. Huang et al. Learning Label Specific Features for Multi-Label Classifcation. ICDM, 2015.
+
+The following package is necessary for executing the program:
+
+LIBSVM -- A Library for Support Vector Machines. 
+URL: http://www.csie.ntu.edu.tw/~cjlin/libsvm.
--- func/MDDM.m
+++ func/MDDM.m
@@ -1,5 +1,22 @@
 function Pre_Labels = MDDM( train_data,train_target,test_data,test_target,percent )
-%MDDM Multi-label Dimension reduction via Dependence Maximization 
+%MDDM Multi-label Dimension reduction via Dependence Maximization [1]
+%
+%    Syntax
+%
+%       Pre_Labels = MDDM(train_data,train_target,test_data,opts)
+%
+%    Description
+%
+%       Input:
+%           train_data       An N x D data matrix, each row denotes a sample
+%           train_target     An L x N label matrix, each column is a label set
+%           test_data        An Nt x D test data matrix, each row is a test sample
+%           percent          Dimensionality of the feature subspace
+% 
+%       Output:
+%           Pre_Labels       An L x Nt predicted label matrix, each column is a predicted label set
+%
+%  [1] Y. Zhang et al. Multilabel dimensionality reduction via dependence maximization. ACM Trans. Knowl. Discov. Data, 2010.
 
 %% Setting parameters
 projtype = 'proj';
