c3a8ae6e46ac6c42bb616a6732c315d9f58da5c3
futuresun912 <futuresun912@gmail.com>
2016-03-31 16:21:48 +0900

    v0.8

--- README.md
+++ README.md
@@ -3,13 +3,7 @@ Multi-Label Classification with Meta-Label Specific Features.
 
 This is an example MATLAB program for conducting experiments in [1].
 
-The program includes five multi-label classification methods:
-
-[1] MLSF: L. Sun et al, Multi-label classification by mining meta-label specific features, a submission to ICPR-16; <br />
-[2] BR: M. Boutel et al, Learning multi-label scene classification, Pattern Recognition, 2004; <br />
-[3] MDDM: Y. Zhang et al, Multilabel dimensionality reduction via dependence maximization; ACM Trans. KDD, 2010; <br />
-[4] LIFT: M. Zhang et al, LIFT: Multi-label learning with label-specific features, IEEE trans. PAMI, 2015; <br />
-[5] LLSF: J. Huang et al, Learning label specific features for multi-label classifcation, ICDM, 2015. <br />
+[1] L. Sun et al, Multi-label classification by mining meta-label specific features, a submission to ICPR-16; <br />
 
 The following package is necessary for executing the program:
 
--- demo.m
+++ demo.m
@@ -16,42 +16,16 @@ rng(1);
 addpath('data','eval');
 addpath(genpath('func'));
 
-%% Specify a MLC method and a dataset
-method   =   'mlsf';
+%% Choose a dataset
 dataset  =   'medical';
 load([dataset,'.mat']);
 
 %% Set parameters 
-svm.type      = 'Linear'; 
-svm.para      = [];
-% MLSF
-mlsf.size     = 10;               
-mlsf.epsilon  = 1e-2; 
-mlsf.alpha    = 0.8;
-mlsf.gamma    = 1e-2;
-mlsf.rho      = 1;
-% MDDM 
-mddm.percent  = 30;
-% LIFT
-lift.ratio    = 0.1;
-% LLSF
-llsf.alpha             = 0.5;
-llsf.beta              = 0.1; 
-llsf.gamma             = 1e-2; 
-llsf.maxIter           = 100;
-llsf.minimumLossMargin = 1e-4;
-llsf.outputtempresult  = 0;
-
-%% Randomly select part of data
-max_num = 8000;
-if size(data,1) > max_num
-    nRows = size(data,1); 
-    nSample = max_num;
-    rndIDX = randperm(nRows);
-    index = rndIDX(1:nSample);
-    data = data(index, :);
-    target = target(:,index);
-end
+opts.size     = 10;               
+opts.epsilon  = 1e-2; 
+opts.alpha    = 0.8;
+opts.gamma    = 1e-2;
+opts.rho      = 1;
 
 %% Perform n-fold cross validation
 num_fold = 5; Results = zeros(5,num_fold);
@@ -59,19 +33,7 @@ indices = crossvalind('Kfold',size(data,1),num_fold);
 for i = 1:num_fold
     disp(['Fold ',num2str(i)]);
     test = (indices == i); train = ~test; 
-    switch method
-        case 'br'
-            tic; Pre_Labels = BRsvm(data(train,:),target(:,train),data(test,:),target(:,test));
-        case 'mddm'
-            tic; Pre_Labels = MDDM(data(train,:),target(:,train),data(test,:),target(:,test),mddm.percent);
-        case 'lift'
-            tic; Pre_Labels = LIFT(data(train,:),target(:,train),data(test,:),target(:,test),lift.ratio,svm);
-        case 'llsf'
-            tic; model_LLSF = LLSF( data(train,:),target(:,train)',llsf);
-            Pre_Labels = LLSF_BR(data(train,:),target(:,train),data(test,:),target(:,test),model_LLSF,svm);
-        case 'mlsf'
-            tic; Pre_Labels = MLSF(data(train,:),target(:,train),data(test,:),mlsf);
-    end
+    tic; Pre_Labels = MLSF(data(train,:),target(:,train),data(test,:),opts);
     Results(1,i) = toc;
     [ExactM,HamS,MacroF1,MicroF1] = Evaluation(Pre_Labels,target(:,test));
     Results(2:end,i) = [ExactM,HamS,MacroF1,MicroF1];
@@ -80,4 +42,4 @@ meanResults = squeeze(mean(Results,2));
 stdResults = squeeze(std(Results,0,2) / sqrt(size(Results,2)));
 
 %% Show the experimental results
-printmat([meanResults,stdResults],[dataset,' by ',method],'Time ExactM HammingS MacroF1 MicroF1','Mean Std.');
+printmat([meanResults,stdResults],dataset,'Time ExactM HammingS MacroF1 MicroF1','Mean Std.');
